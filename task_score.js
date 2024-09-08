72f4364b66ad012e93d69af0390ad71dd9e32d98
ekamva <eka.mva@gmail.com>
2024-09-08 20:17:38 +0300

    HW2.1.4-newName

--- task_score.js
+++ task_score.js
@@ -19,7 +19,13 @@ console.log(`The avarage result is ${averageResult}.`);
 
 //Добавьте переменные с именем и фамилией студента и также добавьте их в вывод к количеству баллов в консоли.
 
-const studentName = "Ivan";
-const studentSurname = "Petrov";
+let studentName = "Ivan";
+let studentSurname = "Petrov";
+let shortstudentName = studentName.slice(0, 1);
 
 console.log(`The avarage result for student ${studentName} ${studentSurname} is ${averageResult}.`);
+
+//В выводе текста в предыдущем задании сократите имя студента до первой буквы, 
+//применив метод slice() к переменной с  именем в команде вывода текста в консоль. 
+
+console.log(`The avarage result for student ${shortstudentName}. ${studentSurname} is ${averageResult}.`);
